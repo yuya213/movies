78351055a53adb0339bb17bfdc5416549f182b3a
yuya213 <129849154+yuya213@users.noreply.github.com@users.noreply.github.com>
2023-06-13 14:24:18 +0000

    Initial Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_10_070849) do
+ActiveRecord::Schema.define(version: 2023_06_13_074418) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 2023_06_10_070849) do
     t.integer "genre_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
   create_table "relationships", force: :cascade do |t|
