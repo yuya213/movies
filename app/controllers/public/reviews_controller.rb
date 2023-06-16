232e4b2a7269d4483b0b27d5e45e2e48868308cf
yuya213 <129849154+yuya213@users.noreply.github.com@users.noreply.github.com>
2023-06-16 08:52:47 +0000

    Commit

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,32 +1,60 @@
 class Public::ReviewsController < ApplicationController
+  before_action :ensure_correct_customer, only: [:edit, :update]
+  
   def edit
+    @review = Review.find(params[:id])
   end
   
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = Review.new
   end
   
   def create
-    @movie = Movie.find(params[:movie_id])
-    @review = @movie.reviews.build(review_params)
+    @review = Review.new(review_params)
+    @review.customer_id = current_customer.id
     if @review.save
-      redirect_to movie_path(@movie), notice: "Review created successfully."
+      redirect_to review_path(@review.id), notice: "Review created successfully."
     else
-      render :new
+      @reviews = Review.all
+      @customer = current_customer
+      render 'index'
     end
+  end
+  
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to review_path(@review), notice: "You have updated review successfully."
+    else
+      @reviews = Review.all
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
   end
 
   def index
+    @reviews = Review.all
   end
 
   def show
+    @review = Review.find(params[:id])
   end
+  
+  def ensure_correct_customer
+    @review = Review.find(params[:id])
+    @customer = @review.customer
+    redirect_to(reviews_path) unless @customer == current_customer
+  end  
   
   private
 
   def review_params
-    params.require(:review).permit(:content, :rating)
+    params.require(:review).permit(:title, :review, :customer_id)
   end
   
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,9 @@ class Review < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
   
+  validates :title, presence: true
+  validates :review, presence: true, length: {maximum:200}
+  
   
   
 end
