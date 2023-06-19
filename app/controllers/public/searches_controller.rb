085925f2328b5423d60bc04b5dbcba4dc7eb084d
yuya213 <129849154+yuya213@users.noreply.github.com@users.noreply.github.com>
2023-06-19 10:49:07 +0000

    Search

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,4 +1,15 @@
 class Public::SearchsController < ApplicationController
+  before_action :authenticate_customer!
+
   def search
+    @range = params[:range]
+    @word = params[:word]
+
+    if @range == "Customer"
+      @customers = Customer.looks(params[:search], params[:word])
+    else
+      @reviews = Review.looks(params[:search], params[:word])
+    end
   end
+  
 end
