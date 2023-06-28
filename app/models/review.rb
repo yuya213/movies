239f91178baf150b9531cbac0e3e532576e62a20
yuya213 <129849154+yuya213@users.noreply.github.com@users.noreply.github.com>
2023-06-28 17:29:06 +0000

    [Fix]

--- app/models/review.rb
+++ app/models/review.rb
@@ -11,6 +11,7 @@ class Review < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: {maximum:200}
+  attribute :body, :text
 
 
   def self.looks(search, word)
