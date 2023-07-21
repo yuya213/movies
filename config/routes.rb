d7be4cb37c32e6c65355decd7d98384d5fe06411
yuya213 <129849154+yuya213@users.noreply.github.com@users.noreply.github.com>
2023-07-21 12:36:38 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
- 
+
   # 会員用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # ゲストログイン用
   # URL /public/guest_sign_in ...
   devise_scope :customer do
@@ -19,25 +19,25 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   # 管理者側
   namespace :admin do
     root to: 'homes#top'
-    resources :customers, only:[:index, :show, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update, :destroy]
     resources :genres, only:[:index, :edit, :create, :update]
     resources :movies, only:[:index, :new, :create, :show, :edit, :update]
     resources :reviews, only:[:show, :update, :destroy] do
      resources :comments, only: [:create, :destroy]
     end
   end
-  
-  
+
+
   # ユーザー側
   scope module: :public do
     root to: 'homes#top'
     get '/about' => 'homes#about'
     get "search" => "searches#search", as: "search"
-    
+
     resources :customers, only:[:index, :show, :edit, :update] do
       resource :relationships, only: [:create, :destroy]
       get 'followings' => 'relationships#followings', as: 'followings'
@@ -45,7 +45,7 @@ Rails.application.routes.draw do
     end
     get "customers/:id/quit" => "customers#quit", as: "quit_customer"
     patch "customers/:id/out" => "customers#out", as: "out_customer"
-    
+
     resources :movies, only: [:index, :show]
     get "searches" => "movies#search", as: "movies_search"
     resources :reviews, only:[:index, :show, :edit, :new, :create, :destroy, :update] do
@@ -55,7 +55,7 @@ Rails.application.routes.draw do
      resources :comments, only: [:create, :destroy]
      resource :favorites, only: [:create, :destroy]
     end
-    
+
   end
 
 end
